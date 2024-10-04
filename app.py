9449b53e9608a3bab26332a5a990415c6e4741b2
Merbau Alexandr <merbau.alex@gmail.com>
2024-10-05 01:12:47 +0700

    всё работает

--- app.py
+++ app.py
@@ -22,23 +22,40 @@ def index():
 def view_code():
     repo_url = request.args.get('repo_url')
     code_html = ""
+    structure_html = ""
 
-    # Создаем временную папку
     with tempfile.TemporaryDirectory() as tmpdirname:
-        Repo.clone_from(repo_url, tmpdirname)  # Клонируем в временную папку
+        Repo.clone_from(repo_url, tmpdirname)
+
+        # Получаем имя проекта
+        project_name = os.path.basename(repo_url).replace('.git', '')
+
+        # Генерация структуры проекта для отображения
+        for root, dirs, files in os.walk(tmpdirname):
+            level = root.replace(tmpdirname, '').count(os.sep)  # Определяем уровень вложенности
+            indent = ' ' * 4 * (level)  # Отступ для текущего уровня
+            if level == 0:  # Если корневой уровень
+                structure_html += f"<strong style='background-color: rgba(255, 255, 0, 0.3);'>{project_name}</strong><br>"  # Название проекта с фоном
+            else:
+                folder_name = os.path.basename(root)
+                structure_html += f"{indent}<strong style='background-color: rgba(0, 255, 0, 0.3);'>{folder_name.upper()}/</strong><br>"  # Папки с фоном
+            for file in files:
+                structure_html += f"{indent}    └── {file}<br>"  # Добавляем файлы
 
         # Генерация кода для отображения
         for root, dirs, files in os.walk(tmpdirname):
             for file in files:
-                if file.endswith('.py'):
-                    file_path = os.path.join(root, file)
+                file_path = os.path.join(root, file)
+                if file.endswith(('.py', '.html', '.txt', '.json', '.yaml', '.yml', '.md')):
                     with open(file_path, 'r', encoding='utf-8') as f:
                         code = f.read()
                         formatter = HtmlFormatter(full=True, linenos=True, cssclass="codehilite")
                         highlighted_code = highlight(code, PythonLexer(), formatter)
-                        code_html += f"<h2>{file}</h2>" + highlighted_code
+                        code_html += f"<h2 class='text-xl font-bold underline'>{project_name}/{file}</h2>" + highlighted_code + "<br><br>"  # Полный путь в заголовке с подчёркиванием
+                elif file.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp')):
+                    code_html += f"<h2 class='text-xl font-bold underline'>{project_name}/{file}</h2><img src='{file_path}' alt='{file}' style='max-width: 100%;'><br><br>"  # Подчёркивание и путь для изображений
 
-    return render_template('view_code.html', code_html=code_html, repo_url=repo_url)
+    return render_template('view_code.html', code_html=code_html, structure_html=structure_html, repo_url=repo_url)
 
 if __name__ == '__main__':
     app.run(debug=True)
